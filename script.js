5b1aa126f344c8eb80d4b307f498329a3fb55d0c
ATiramisu <130720803+ATiramisu@users.noreply.github.com>
2023-04-15 22:25:52 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+function convert() {
+
+}
+
+function convertandsave() {
+    saveToFile(result, "IEEE-754.txt");
+}
+
+function decimalToBinary(decimal, rounding) {
+  
+}
+    
+        function binaryToHexadecimal(binary) {
+
+        }
+    
+        function decToBin(decimal, length) {
+            let binary = decimal.toString(2);
+            while (binary.length < length) {
+                binary = "0" + binary;
+            }
+            return binary;
+        }
+    
+        function binToDec(binary) {
+            return parseInt(binary, 2);
+        }
+    
+        function decToHex(decimal) {
+            return decimal.toString(16).toUpperCase();
+        }
+    
+		function saveToFile(data, filename) {
+			// Create blob with binary data
+			let blob = new Blob([data], {type: "application/octet-stream"});
+
+			// Create URL for blob
+			let url = URL.createObjectURL(blob);
+
+			// Create link element for download
+			let link = document.createElement("a");
+			link.href = url;
+			link.download = filename || "IEEE-754.txt";
+
+			// Click link to start download
+			link.click();
+
+			// Clean up
+			URL.revokeObjectURL(url);
+		}
